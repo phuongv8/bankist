5827993e56b62892492df462ef22d1d4866c3073
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2022-12-11 18:05:54 -0800

    Display date and time when user logs in

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const movementsContainer = document.querySelector(".movements");
 
 const welcomeLabel = document.querySelector(".welcome");
 const timerLabel = document.querySelector(".timer");
-const balanceDate = document.querySelector(".date");
+const dateLabel = document.querySelector(".date");
 const balanceValue = document.querySelector(".balance-value");
 
 const summaryIn = document.querySelector(".summary-value-in");
@@ -29,32 +29,61 @@ const inputClosePassword = document.querySelector(".form-input-password");
 const account1 = {
   fullName: "Apple Pie",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movementsDates: [
+    "2021-11-18T21:31:17.178Z",
+    "2021-12-23T07:42:02.383Z",
+    "2022-01-28T09:15:04.904Z",
+    "2022-04-01T10:17:24.185Z",
+    "2022-05-08T14:11:59.604Z",
+    "2022-07-26T17:01:17.194Z",
+    "2022-07-28T23:36:17.929Z",
+    "2022-08-01T10:51:36.790Z",
+  ],
   username: "ap",
   password: 1111,
 };
 const account2 = {
   fullName: "Banana Chip",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  movementsDates: [
+    "2021-11-18T21:31:17.178Z",
+    "2021-12-23T07:42:02.383Z",
+    "2022-01-28T09:15:04.904Z",
+    "2022-04-01T10:17:24.185Z",
+    "2022-05-08T14:11:59.604Z",
+    "2022-07-26T17:01:17.194Z",
+    "2022-07-28T23:36:17.929Z",
+    "2022-08-01T10:51:36.790Z",
+  ],
   username: "bc",
   password: 2222,
 };
 const account3 = {
   fullName: "Coconut Milk",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  movementsDates: [
+    "2021-11-18T21:31:17.178Z",
+    "2021-12-23T07:42:02.383Z",
+    "2022-01-28T09:15:04.904Z",
+    "2022-04-01T10:17:24.185Z",
+    "2022-05-08T14:11:59.604Z",
+    "2022-07-26T17:01:17.194Z",
+    "2022-07-28T23:36:17.929Z",
+    "2022-08-01T10:51:36.790Z",
+  ],
   username: "cm",
   password: 3333,
 };
-const account4 = {
-  fullName: "Mango Juice",
-  movements: [430, 1000, 700, 50, 90],
-  username: "mj",
-  password: 4444,
-};
 
-let accounts = [account1, account2, account3, account4];
+let accounts = [account1, account2, account3];
 let userMovements;
 let currentAccount;
 let sorted = false;
+
+// TODO Stay logged in - delete later!
+currentAccount = account1;
+updateUI(currentAccount);
+appContainer.style.opacity = 100;
 
 function displayMovements(movements, sorted = false) {
   movementsContainer.innerHTML = "";
@@ -120,6 +149,14 @@ function login(e) {
     welcomeLabel.textContent = `Welcome back, ${currentAccount.fullName}`;
     inputLoginPassword.value = inputLoginUsername.value = "";
     updateUI(currentAccount);
+
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0); // print format: 02/08/2022
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hour = now.getHours();
+    const min = now.getMinutes();
+    dateLabel.textContent = `${month}/${day}/${year}, ${hour}:${min}`;
   } else {
     // TODO Error message
     console.log("wrong username or password/account does not exist");
