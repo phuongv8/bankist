dd8f7c540f92cd6d8ae454e1a61818bf69401981
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2022-12-12 19:19:30 -0800

    Add date format for recent dates

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ const account1 = {
     "2021-12-23T07:42:02.383Z",
     "2022-01-28T09:15:04.904Z",
     "2022-04-01T10:17:24.185Z",
-    "2022-05-08T14:11:59.604Z",
-    "2022-07-26T17:01:17.194Z",
-    "2022-07-28T23:36:17.929Z",
-    "2022-08-01T10:51:36.790Z",
+    "2022-12-01T14:11:59.604Z",
+    "2022-12-08T17:01:17.194Z",
+    "2022-12-10T23:36:17.929Z",
+    "2022-12-11T10:51:36.790Z",
   ],
   username: "ap",
   password: 1111,
@@ -80,10 +80,20 @@ let userMovements;
 let currentAccount;
 let sorted = false;
 
-// TODO Stay logged in - delete later!
-currentAccount = account1;
-updateUI(currentAccount);
-appContainer.style.opacity = 100;
+const formatDate = function (date) {
+  const dayPassed = Math.round(
+    Math.abs(new Date() - date) / (1000 * 60 * 60 * 24)
+  );
+
+  if (dayPassed == 0) return "Today";
+  else if (dayPassed <= 7) return `${dayPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0); // print format: 02/08/2022
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+  }
+};
 
 function displayMovements(account, sorted = false) {
   movementsContainer.innerHTML = "";
@@ -96,10 +106,8 @@ function displayMovements(account, sorted = false) {
     const type = move > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(account.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0); // print format: 02/08/2022
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${month}/${day}/${year}`;
+    const displayDate = formatDate(date);
+    console.log("displayDate", displayDate);
 
     const html = `
         <div class="movements-row">
